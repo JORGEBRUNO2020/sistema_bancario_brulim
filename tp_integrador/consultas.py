90ff8409cc151990a81924920c00c5d324d13a64
Marcelo Limideiro <mlimideiro>
2022-08-30 19:35:30 -0300

    conexion lograda bd

--- tp_integrador/consultas.py
+++ tp_integrador/consultas.py
@@ -2,18 +2,19 @@ import mysql.connector
 
 class Banco:
     def __init__(self):
-        self.cnn = mysql.connector.connect(host="LocalHost", user = "root", passwd = "", database = "sistema_bancario")
+        self.cnn = mysql.connector.connect(host="localhost", user = "root", passwd = "", database = "sistema_banco")
 
     def __str__(self):
         datos = self.consulta()
-        i = ""
-        for row in datos:
-            i = i + str(row) + "\n"
-        return i
+        # i = ""
+        # for row in datos:
+        #     i = i + str(row) + "\n"
+        # return i
+        return print(datos)
 
     def consulta(self):
         cur = self.cnn.cursor()
-        cur.execute("SELECT * FROM `cliente` WHERE 1")
+        cur.execute("SELECT * FROM 'cliente'")
         datos = cur.nombre()
         cur.close()
         return datos
@@ -21,3 +22,5 @@ class Banco:
 
 banco = Banco()
 print(banco.consulta)
+
+
