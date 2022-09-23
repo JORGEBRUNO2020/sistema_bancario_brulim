862dc8e69067a8797255903aea48f630144b4da4
Marcelo Limideiro <mlimideiro@gmail.com>
2022-09-23 20:35:55 -0300

    test conexion 2

--- Connection/app01.py
+++ Connection/app01.py
@@ -1,10 +1,38 @@
 from flask import Flask, render_template
+import mysql.connector
+
+connection = mysql.connector.connect(host='localhost',
+                                        database='sistema_bancario',
+                                        user='root',
+                                        password='')
+
+sql_select_Query = "select * from sucursal"
+cursor = connection.cursor()
+cursor.execute(sql_select_Query)
+# get all records
+records = cursor.fetchall()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 
+
+
 def index():
-    usuario_banco = "marce limi"
+    #usuario_banco = "marce limi"
+    for row in records:
+        print("Id = ", row[0], )
+        print("Name = ", row[1])
+        print("Apellido  = ", row[2])
+        # print("Telefono  = ", row[4])
+        # print("mail  = ", row[5])
+        # print("Usuario  = ", row[6])
+        # print("Password  = ", row[7],"\n")
+    return row
+
+
+
+if __name__ == "__main__":
+    app.run()
 
--- test_conect/app.py
+++ test_conect/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET","POST"])
+
 def main():
     # When it is a GET
     if request.method == "GET":
