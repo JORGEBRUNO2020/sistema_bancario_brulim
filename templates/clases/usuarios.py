4ca59a3085378e647d5022388c4c5b9f2461e2fc
Jorge Bruno <jorgeluisbruno2020@gmail.com>
2022-10-22 21:21:26 -0300

    carpeta clases

---
+++ templates/clases/usuarios.py
@@ -0,0 +1,22 @@
+class Usuario():
+
+    def __init__(self,nombre_usuario, password):
+        self.__nombre = nombre_usuario
+        self.__password = password
+
+    def __str__(self):
+        return ("Nombre de usuario: " + str(self.__nombre) + "\nPassword: " + str(self.__password))
+
+    def get_nombre_usuario():
+        pass
+
+    def get_password():
+        pass
+
+    def set_password():
+        pass
+        
+    def set_nombre_usuario():
+        pass
+
+
