49c4267efeecece79374687e966a820347386558
Marcelo Limideiro <mlimideiro@gmail.com>
2022-10-22 21:49:21 -0300

    rearmando carpetas

--- varios/tp_fantasia/clientes.py
+++
@@ -1,7 +0,0 @@
-class Clientes():
-    def __init__(self, domicilio, cuitCuil, telefono, email):
-        self.domicilio = domicilio
-        self.cuitCuil = cuitCuil
-        self.telefono = telefono
-        self.email = email
-      
--- varios/tp_fantasia/saldoretenido.py
+++
@@ -1,3 +0,0 @@
-class SaldoRetenido():
-    def __init__(self, monto_retenido):
-        self.monto_retenido = monto_retenido
